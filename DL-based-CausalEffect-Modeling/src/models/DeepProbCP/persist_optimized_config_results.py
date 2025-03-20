e5f568331aeacab4ceba4e0df1011f629acd02aa
Orri11 <orshahar66@gmail.com>
2025-03-20 15:59:13 +0100

    organized files

--- DL-based-CausalEffect-Modeling/src/models/DeepProbCP/persist_optimized_config_results.py
+++
@@ -1,7 +0,0 @@
-def persist_results(results, file):
-    file_object = open(file, mode = 'w')
-
-    for k, v in results.items():
-        file_object.write(str(k) + ' >>> ' + str(v) + '\n\n')
-
-    file_object.close()
